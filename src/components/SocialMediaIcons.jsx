40e53848a0185619f10f51772dc9d54a115f27ad
Alex <mr.shaljyan@gmail.com>
2023-02-05 00:54:08 +0400

    MM

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,45 @@
+
+const SocialMediaIcons = () => {
+return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+<a
+className="hover:opacity-50 transition duration-500"
+href="https://www.linkedin.com"
+targer="_blank"
+rel="noreferrer"
+>
+<img alt="linkedin-Link" src="../assets/Linkedin.png" />
+</a>
+
+<a
+className="hover:opacity-50 transition duration-500"
+href="https://www.twitter.com"
+targer="_blank"
+rel="noreferrer"
+>
+<img alt="twitter-Link" src="../assets/twitter.png" />
+</a>
+
+<a
+className="hover:opacity-50 transition duration-500"
+href="https://www.facebook.com"
+targer="_blank"
+rel="noreferrer"
+>
+<img alt="facebook-Link" src="../assets/facebook.png" />
+</a>
+
+<a
+className="hover:opacity-50 transition duration-500"
+href="https://www.instagram.com"
+targer="_blank"
+rel="noreferrer"
+>
+<img alt="instagram-Link" src="../assets/instagram.png" />
+</a>
+    </div>
+)
+
+}
+
+export default SocialMediaIcons;
